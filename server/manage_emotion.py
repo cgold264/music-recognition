5f3c9f771bdc47932ee0707417f55ff8c5a259ad
Alexa Dabai <alexadabai@MacBook-Air-9.local>
2024-04-08 11:18:08 -0700

    alexa changes

---
+++ server/manage_emotion.py
@@ -0,0 +1,20 @@
+
+# angry
+# neutral
+# happy
+# fear
+# surprise
+# sad
+def choose_genre(emotion):
+    if(emotion == "happy"):
+        print("country")
+        return "country"
+    else:
+        return "country"
+
+#array of emotions
+def choose_genre_mult_emotion(emotions):
+    return 
+
+
+choose_genre("happy")
